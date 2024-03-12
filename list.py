3f36fcecf1efa18240f5a21cf17381dcf6e7e66f
Markayo21 <159958226+Markayo21@users.noreply.github.com>
2024-03-12 23:39:41 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+my_list = []
+my_list.extend([10,20,30,40])
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort
+index = my_list.index(30)
+print(my_list)
+print(f"The index of 30 is :{index}")
+
